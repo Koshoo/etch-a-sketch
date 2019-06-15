27f41778ed6a8949ba2658d7f99b112e685511be
Koshoo <benkosh@gmail.com>
2019-06-15 05:11:59 +0300

    final version

--- index.js
+++ index.js
@@ -1,41 +1,97 @@
+const resetButton = document.getElementById("reset-btn");
+const randomButton = document.getElementById("random-btn");
+const blackButton = document.getElementById("black-btn");
+createGrid(16);
 
 function createGrid(gridSize) {
-	for (let i = 0; i < gridSize; i++) {
+	let gridContainer = document.querySelector('.grid-container');
+	let gridCells = document.getElementsByClassName('grid-cell');
+	let gridCellsArr = Array.from(gridCells);
 
-		for (let j = 1; j < gridSize + 1; j++) {
-			var gridCell = document.createElement("div");
-			gridCell.classList.add('grid-cell');
-			gridCell.style.gridColumn = '0' + j;
-			gridContainer.appendChild(gridCell);
-		}
+	//clearing gridContainer if its not clear, before creating a new one.
+	while (gridContainer.firstChild) {
+		gridContainer.firstChild.remove();
 	}
-}
 
-function clearGrid() {
-	gridCellsArr.forEach(function (element) {
-		element.classList.remove("painted");
+	gridContainer.style.gridTemplateColumns = "1fr ".repeat(gridSize);
+	gridContainer.style.gridTemplateRows = "1fr ".repeat(gridSize);
+
+	//creating grid cells
+
+	for (let j = 0; j < (gridSize * gridSize); j++) {
+		var gridCell = document.createElement("div");
+		gridCell.classList.add('grid-cell');
+
+		gridContainer.appendChild(gridCell);
+	}
+
+	gridCells = document.getElementsByClassName('grid-cell');
+	gridCellsArr = Array.from(gridCells);
+
+	gridCellsArr.forEach(function (cell) {
+		cell.addEventListener("mouseover", function () {
+			cell.classList.add("painted");
+		});
+		cell.addEventListener("click", function () {
+			clearCell(cell);
+		});
 	});
 }
 
-const gridContainer = document.querySelector('.grid-container');
-createGrid(16);
-
-const gridCells = document.getElementsByClassName('grid-cell');
-const gridCellsArr = Array.from(gridCells);
-const resetButton = document.getElementById("reset-btn");
-
 resetButton.addEventListener("click", function () {
-	clearGrid();
+	let size = prompt("How many squares? (for 16*16 type 16) from 1 to 100");
+	if(size ===null){
+		return;
+	}
+	while (size < 1 || size > 100) {
+		size = prompt("How many squares? (for 16*16 type 16) from 1 to 100");
+	}
+	createGrid(size);
 });
 
-gridCellsArr.forEach(function (cell) {
-	cell.addEventListener("mouseover", function () {
-		cell.classList.add("painted");
-	})
+randomButton.addEventListener("click", function () {
+	randomizeColor();
 });
-gridCellsArr.forEach(function (cell) {
-	cell.addEventListener("ontouchstart", function () {
-		cell.classList.add("painted");
-	})
+
+blackButton.addEventListener("click", function() {
+	blackColor();
 });
+
+function getRandom() {
+	return "rgb(" + parseInt(Math.floor(Math.random() * 256)) + "," + parseInt(Math.floor(Math.random() * 256)) + "," + parseInt(Math.floor(Math.random() * 256)) + ")";
+}
+
+function randomizeColor() {
+	gridCells = document.getElementsByClassName('grid-cell');
+	gridCellsArr = Array.from(gridCells);
+	gridCellsArr.forEach(function (cell) {
+		cell.addEventListener("mouseover", function () {
+			cell.classList.remove("painted");
+			cell.style.backgroundColor = getRandom();
+		});
+	});
+}
+
+function clearCell(cell) {
+	cell.classList.remove("painted");
+	cell.style.backgroundColor = "white";
+}
+
+function blackColor(){
+	gridCells = document.getElementsByClassName('grid-cell');
+	gridCellsArr = Array.from(gridCells);
+	gridCellsArr.forEach(function (cell) {
+		cell.addEventListener("mouseover", function () {
+			cell.classList.add("painted");
+			cell.style.backgroundColor = getRandom();
+		});
+	});
+}
+
+
+
+
+
+
+
 
